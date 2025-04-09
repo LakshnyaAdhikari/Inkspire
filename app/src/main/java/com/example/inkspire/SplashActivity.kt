3fd6eaa078aaf04f065a8ed8ab08e8f57e215ba6
kritika014 <hellokritika2006@gmail.com>
2025-04-09 12:59:23 +0530

    inkspire

--- app/src/main/java/com/example/inkspire/SplashActivity.kt
+++ app/src/main/java/com/example/inkspire/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.example.inkspire
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.igdtuw.inkspire.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
