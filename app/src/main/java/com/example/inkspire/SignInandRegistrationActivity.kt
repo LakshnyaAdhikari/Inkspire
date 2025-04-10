05c67f050308134dbd9bbc26bad23225e80dbcc4
kritika014 <hellokritika2006@gmail.com>
2025-04-10 20:26:37 +0530

    inkspire

--- app/src/main/java/com/example/inkspire/SignInandRegistrationActivity.kt
+++ app/src/main/java/com/example/inkspire/SignInandRegistrationActivity.kt
@@ -2,24 +2,28 @@ package com.example.inkspire
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.inkspire.databinding.ActivitySignInandRegistrationBinding
 import com.example.inkspire.databinding.ActivityWelcomeBinding
+import java.net.Authenticator
 import javax.security.auth.login.LoginException
 
 class SignInandRegistrationActivity : AppCompatActivity() {
     private val binding: ActivitySignInandRegistrationBinding by lazy {
         ActivitySignInandRegistrationBinding.inflate(layoutInflater)
     }
-
-
+    private lateinit var auth:FireBaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        //initialize firebase authentication
+
+        auth= FireBaseAuth.getInstance()
 
         val action:String? = intent.getStringExtra("action")
         //adjust visibility for login
@@ -53,6 +57,26 @@ binding.Loginbutton.visibility= View.VISIBLE
         else if (action=="Signup"){
             binding.Loginbutton.isEnabled=false
             binding.Loginbutton.alpha=0.5f
+            binding.registrationbutton.setonClickListener{
+                //get data from edit text field
+                val registerName=binding.registerName.text.toString()
+                val register_email=binding.registerName.text.toString()
+                val register_password=binding.registerName.text.toString()
+                if( registerName.isEmpty() || register_email.isEmpty() ||register_password.isEmpty()){
+                    Toast.makeText(this, text:"please fill all the details", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(register_email,register_password)
+                        .addOnCompleteListener { task->
+                            if(task.issuccessful){
+
+                            }
+                            else{
+
+                            }
+                        }
+                }
+            }
 
         }
 
