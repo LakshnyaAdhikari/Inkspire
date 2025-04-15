364c8a51fc74e08f59998d9369480370542ef639
kritika014 <hellokritika2006@gmail.com>
2025-04-15 21:02:11 +0530

    inkspire

---
+++ app/src/main/java/com/example/inkspire/adapter/blogadapter.kt
@@ -0,0 +1,12 @@
+package com.example.inkspire.adapter
+
+import android.widget.RemoteViews.RemoteCollectionItems
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inkspire.Model.BlogItemAdded
+
+class blogadapter (private val items: List<BlogItemAdded>):RecyclerView.Adapter<blogadapter.blogViewHolder>{
+   inner class blogViewHolder {
+       
+    }
+
+}
