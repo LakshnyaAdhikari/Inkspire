71853b4908a8ffe0ffd37f7e7f262390ae55e160
Khwaaish <khwaaishdiwan@gmail.com>
2025-04-15 21:03:35 +0530

    inkspire

---
+++ app/src/main/java/com/example/inkspire/Model/BlogItemModel.kt
@@ -0,0 +1,9 @@
+package com.example.inkspire.Model
+
+data class BlogItemModel(
+    val heading: String,
+    val username: String,
+    val date:String,
+    val post:String,
+    val likecount:Int
+)
