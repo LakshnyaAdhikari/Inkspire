06913bf0c07684e0b648df6ece8265652d4a061d
kritika014 <hellokritika2006@gmail.com>
2025-04-15 20:53:00 +0530

    inkspire

---
+++ app/src/main/java/com/example/inkspire/Model/BlogItemAdded.kt
@@ -0,0 +1,10 @@
+package com.example.inkspire.Model
+
+class BlogItemAdded {
+    val heading: String ,
+    val UserName: String ,
+    val date:String ,
+    val post:String,
+    val likeCount:Int,
+
+}
