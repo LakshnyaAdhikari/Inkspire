f58fd6f502eb6aa94ed61963a5ecda19a5c9eb1c
admin <lakshnyaadhikari1@gmail.com>
2025-04-16 00:28:47 +0530

    Inkspire

---
+++ app/src/main/java/com/example/inkspire/adapter/BlogAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.inkspire.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inkspire.Model.BlogItemModel
+import com.example.inkspire.databinding.BlogItemBinding
+
+class BlogAdapter (private val items: List<BlogItemModel>):RecyclerView.Adapter<BlogAdapter.blogViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): blogViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val binding:BlogItemBinding=BlogItemBinding.inflate(inflater, parent, false)
+        return blogViewHolder(binding)
+    }
+
+
+
+    override fun onBindViewHolder(holder: blogViewHolder, position: Int) {
+      holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+     return items.size    }
+
+    inner class blogViewHolder (private val binding: BlogItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(blogItemModel: BlogItemModel) {
+          binding.heading.text=BlogItemModel.heading
+            binding.profile.setImageResource()
+
+            binding.username.text=blogItemModel.username
+            binding.date.text=blogItemModel.date
+            binding.post.text=blogItemModel.post
+            binding.likecount.text=blogItemModel.likecount.toString()
+
+
+        }
+
+    }
+
+}
