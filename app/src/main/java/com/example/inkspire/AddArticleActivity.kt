63fb160c1f83eb36f96b549e4845e82931192992
kritika014 <hellokritika2006@gmail.com>
2025-04-16 21:53:15 +0530

    inkspire

--- app/src/main/java/com/example/inkspire/AddArticleActivity.kt
+++ app/src/main/java/com/example/inkspire/AddArticleActivity.kt
@@ -2,14 +2,19 @@ package com.example.inkspire
 
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.SimpleAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.inkspire.Model.UserData
 import com.example.inkspire.databinding.ActivityAddArticleBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddArticleActivity : AppCompatActivity() {
     private val binding:ActivityAddArticleBinding by lazy {
@@ -29,8 +34,46 @@ class AddArticleActivity : AppCompatActivity() {
             insets
 
             binding.addBlogButton.setOnClickListener{
+             val title:String= binding.blogTitle.editText?.text.toString().trim()
+             val description:String= binding.blogDescription.editText?.text.toString().trim()
 
+              if(title.isEmpty() || description.isEmpty())  {
+                  Toast.makeText(context:this , text:"please fill all the fields",Toast.LENGTH_SHORT).show()
+              }
+               val user:FirebaseUser?: auth.currentUser
+               if(user!=null){
+                   val userId:String = user.uid
+                   val userName:String= user.displayName?"Anonymous"
+                   val userImageUrl:comparable<{String& uri}>=user.photoUrl?:""
+
+                   userReference.child(userId).addListenerForSingleValueEvent(object: ValueEventListener{
+                       override fun onDatachange(snapshot:DataSnapshot){
+
+                       }
+                       override fun onCancelled(error:DatabaseError){
+                           val userData:Any? = snapshot.getValue(UserData::class.java)
+                           if(UserData!=null){
+                               val UserNameFromDB:String= userData.name
+                               val userImageUrlFromDB=UserData.profileImage
+
+                               val currentDate = SimpleDateFormat(pattern:"yyyy-mm-dd").format(Date())
+
+                               val blogItem = blogItemModel(
+                                   title,
+                                   userImageUrlFromDB,
+                                   currentDate,
+                                   likecount:0,
+                                   description,
+                                   userImageUrl,
+
+
+                               )
+                           }
+                       }
+                   })
+               }
             }
         }
+        
     }
 }
--- app/src/main/java/com/example/inkspire/Model/UserData.kt
+++ app/src/main/java/com/example/inkspire/Model/UserData.kt
@@ -3,6 +3,11 @@ package com.example.inkspire.Model
 import android.provider.ContactsContract.CommonDataKinds.Email
 
 data class UserData(
-    val name: String,
-    val email: String
-)
+    val name: String="",
+    val email: String="",
+    val profileImage:String="",
+    val likecount:Int,
+    val imageUrl:String
+){
+    constructor():this(name="",email="",profileImage="")
+}
