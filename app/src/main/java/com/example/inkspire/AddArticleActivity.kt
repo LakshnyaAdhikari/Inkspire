7ee9271e872c23f6093f76e398830121c9d42478
admin <lakshnyaadhikari1@gmail.com>
2025-04-16 14:50:07 +0530

    Inkspire

--- app/src/main/java/com/example/inkspire/AddArticleActivity.kt
+++ app/src/main/java/com/example/inkspire/AddArticleActivity.kt
@@ -1,20 +1,37 @@
 package com.example.inkspire
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.inkspire.databinding.ActivityAddArticleBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AddArticleActivity : AppCompatActivity() {
+    private val binding:ActivityAddArticleBinding by lazy {
+        ActivityAddArticleBinding.inflate(layoutInflater)
+    }
+
+    private val databaseReference: DatabaseReference=FirebaseDatabase.getInstance().getReference("blogs")
+    private val userReference: DatabaseReference=FirebaseDatabase.getInstance().getReference("users")
+    private val auth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_article)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            binding.addBlogButton.setOnClickListener{
+
+                val title: EditText? =binding.blogTitle.editText?.text.toString().trin()
+            }
         }
     }
 }
