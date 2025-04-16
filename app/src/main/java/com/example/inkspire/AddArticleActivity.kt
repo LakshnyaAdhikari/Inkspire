71fb65cb796e918b1abd6e981d37d89497b45352
Khwaaish <khwaaishdiwan@gmail.com>
2025-04-16 08:32:43 +0530

    inkspire

--- app/src/main/java/com/example/inkspire/AddArticleActivity.kt
+++ app/src/main/java/com/example/inkspire/AddArticleActivity.kt
@@ -1,26 +1,16 @@
 package com.example.inkspire
 
-import android.content.Intent
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.inkspire.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+class AddArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        binding.floatingAddArticleButton.setOnClickListener {
-            startActivity(Intent(this,AddArticleActivity::class.java))
-        }
+        setContentView(R.layout.activity_add_article)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
