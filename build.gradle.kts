f2bdbaba7db96e04cfbbcdeb4eef2eb72ee0e729
admin <lakshnyaadhikari1@gmail.com>
2025-04-17 22:57:46 +0530

    Inkspire

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.services) apply false
-    id("com.android.application")
-    id("com.google.gms.google-services")
+
 
 
 }
