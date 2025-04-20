05c4da82d8ba06f562deb3e2104f81e1eb675dee
Khwaaish <khwaaishdiwan@gmail.com>
2025-04-20 22:22:14 +0530

    inkspire

--- app/src/main/java/com/example/inkspire/AddArticleActivity.kt
+++ app/src/main/java/com/example/inkspire/AddArticleActivity.kt
@@ -31,9 +31,9 @@ class AddArticleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-//    binding.imageButton.setOnClickListener{
-//        finish()
-//    }
+    binding.backButton2.setOnClickListener{
+       finish()
+    }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
